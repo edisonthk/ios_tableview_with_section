d5a10506eafab2bf4c2128c952545f0a31ab95a4
edisonthk-mac <edisonthk@gmail.com>
2014-05-20 02:38:35 +0900

    Initial Commit

---
+++ TableLayoutLesson/MWAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  MWAppDelegate.h
+//  TableLayoutLesson
+//
+//  Created by Edisonthk on 5/20/14.
+//  Copyright (c) 2014 Maizework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TableLayoutLessonTests/TableLayoutLessonTests.m
@@ -0,0 +1,34 @@
+//
+//  TableLayoutLessonTests.m
+//  TableLayoutLessonTests
+//
+//  Created by Edisonthk on 5/20/14.
+//  Copyright (c) 2014 Maizework. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableLayoutLessonTests : XCTestCase
+
+@end
+
+@implementation TableLayoutLessonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
